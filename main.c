ee6f5c9ecc40bf79b4327a7ce380f7ee6bc256b6
EtudiantTest <nathalie.neil@aliceadsl.fr>
2026-01-07 14:03:41 +0100

    Add if-else statement to check positivity of n

--- main.c
+++ main.c
@@ -6,6 +6,13 @@ int main(void)
     scanf("%d", &n);
 
     /* TODO : test if / else */
-
+    if (n>=0)
+    {
+        printf("POSITIF");
+    }
+    else
+    {
+        printf("Toto fait du vélo");
+    }
     return 0;
 }
