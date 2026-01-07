e85db4f4435d0a2b96992f5fe5d376353413b4f6
EtudiantTest <nathalie.neil@aliceadsl.fr>
2026-01-07 07:23:30 +0100

    Add if-else statement to check positive number

--- main.c
+++ main.c
@@ -6,6 +6,9 @@ int main(void)
     scanf("%d", &n);
 
     /* TODO : test if / else */
-
+    if (n>=0)
+        {printf(POSITIF");}
+    else
+        {printf("Toto fait du vélo");}
     return 0;
 }
