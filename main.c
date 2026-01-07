394c63f8511a8686225b667ffcbfc61614a8dfef
EtudiantTest <nathalie.neil@aliceadsl.fr>
2026-01-07 14:11:45 +0100

    Update comment to reflect version change

--- main.c
+++ main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
-
+// Push v4 7janvier25
 int main(void)
 {
     int n;
     scanf("%d", &n);
 
     /* TODO : test if / else */
-
+    if (n>=0)
+    {
+        printf("POSITIF");
+    }
+    else
+    {
+        printf("NEGATIF");
+    }
     return 0;
 }
